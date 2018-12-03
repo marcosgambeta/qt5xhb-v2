da46dc5c5db48a40dcf193286f2d131f264e3449
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2018-12-03 17:06:33 -0200

    Added class Signals2
    Modified:
    makefile_android
    makefile_clang
    makefile_linux
    makefile_mingw
    makefile_msvc
    
    Added:
    include/qt5xhb_signals2.h
    source/signals2.cpp
    source/signals2.h

---
+++ include/qt5xhb_signals2.h
@@ -0,0 +1,23 @@
+/*
+
+  Qt5xHb - Bindings libraries for Harbour/xHarbour and Qt Framework 5
+
+  Copyright (C) 2018 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
+
+*/
+
+#ifndef QT5XHB_SIGNALS2_H
+#define QT5XHB_SIGNALS2_H
+
+bool Signals2_connect_signal ( QObject * object, QString signal, PHB_ITEM codeblock );
+bool Signals2_disconnect_signal ( QObject * object, QString signal );
+bool Signals2_is_signal_connected( QObject * object, QString signal );
+PHB_ITEM Signals2_return_codeblock ( QObject * object, QString signal );
+void Signals2_release_codeblocks ();
+void Signals2_disconnect_all_signals (QObject * obj);
+bool Signals2_connection ( QObject * s, QString signal );
+bool Signals2_disconnection ( QObject * s, QString signal );
+PHB_ITEM Signals2_return_object ( void * ptr, const char * classname );
+PHB_ITEM Signals2_return_qobject ( QObject * ptr, const char * classname );
+
+#endif /* QT5XHB_SIGNALS2_H */
---
+++ source/signals2.h
@@ -0,0 +1,53 @@
+/*
+
+  Qt5xHb - Bindings libraries for Harbour/xHarbour and Qt Framework 5
+
+  Copyright (C) 2018 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
+
+*/
+
+#ifndef SIGNALS2_H
+#define SIGNALS2_H
+
+#include <QObject>
+#include <QMetaObject>
+#include <QMetaMethod>
+#include <QVector>
+#include <QList>
+#include <QCoreApplication>
+#include <QString>
+#include <QMutex>
+
+#include "hbapi.h"
+#include "hbapiitm.h"
+#include "hbapierr.h"
+#include "hbvm.h"
+#include "hbstack.h"
+#ifndef __XHARBOUR__
+#include "hbapicls.h"
+#endif
+
+bool Signals2_connect_signal ( QObject * object, QString signal, PHB_ITEM codeblock );
+bool Signals2_disconnect_signal ( QObject * object, QString signal );
+bool Signals2_is_signal_connected ( QObject * object, QString signal );
+PHB_ITEM Signals2_return_codeblock ( QObject * object, QString signal );
+void Signals2_release_codeblocks ();
+void Signals2_disconnect_all_signals (QObject * obj, bool children);
+bool Signals2_connection ( QObject * s, QString signal );
+bool Signals2_disconnection ( QObject * s, QString signal );
+PHB_ITEM Signals2_return_object ( void * ptr, const char * classname );
+PHB_ITEM Signals2_return_qobject ( QObject * ptr, const char * classname );
+
+class Signals2: public QObject
+{
+  public:
+  Signals2(QObject *parent = 0);
+  ~Signals2();
+  QVector<QObject*> list1; // armazena ponteiro do objeto
+  QVector<QString> list2;  // armazena assinatura do sinal
+  QVector<PHB_ITEM> list3; // armazena codeblock
+  QVector<bool> list4;     // ativo (true) ou liberado (false)
+  QMutex * mutex;
+};
+
+#endif
