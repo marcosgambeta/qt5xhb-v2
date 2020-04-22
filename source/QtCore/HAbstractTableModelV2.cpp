57831156ea086e1c08614b4b78e72095426604e2
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2020-04-22 01:47:38 -0300

    HAbstractTableModelV2: class updated
    Modified:
    source/QtCore/HAbstractTableModelV2.cpp
    source/QtCore/HAbstractTableModelV2.h
    source/QtCore/HAbstractTableModelV2Class.prg

--- source/QtCore/HAbstractTableModelV2.cpp
+++ source/QtCore/HAbstractTableModelV2.cpp
@@ -11,130 +11,130 @@
 /*
   método construtor
 */
-HAbstractTableModelV2::HAbstractTableModelV2(QObject * parent) : QAbstractTableModel(parent)
+HAbstractTableModelV2::HAbstractTableModelV2( QObject * parent ) : QAbstractTableModel( parent )
 {
-  rowCountBlock = nullptr;
-  columnCountBlock = nullptr;
-  dataBlock = nullptr;
-  headerDataBlock = nullptr;
-  flagsBlock = nullptr;
-  setDataBlock = nullptr;
+  m_rowCountBlock = nullptr;
+  m_columnCountBlock = nullptr;
+  m_dataBlock = nullptr;
+  m_headerDataBlock = nullptr;
+  m_flagsBlock = nullptr;
+  m_setDataBlock = nullptr;
 }
 
 HAbstractTableModelV2::~HAbstractTableModelV2()
 {
-  if( rowCountBlock != nullptr )
+  if( m_rowCountBlock != nullptr )
   {
-    hb_itemRelease( rowCountBlock );
-    rowCountBlock = nullptr;
+    hb_itemRelease( m_rowCountBlock );
+    m_rowCountBlock = nullptr;
   }
 
-  if( columnCountBlock != nullptr )
+  if( m_columnCountBlock != nullptr )
   {
-    hb_itemRelease( columnCountBlock );
-    columnCountBlock = nullptr;
+    hb_itemRelease( m_columnCountBlock );
+    m_columnCountBlock = nullptr;
   }
 
-  if( dataBlock != nullptr )
+  if( m_dataBlock != nullptr )
   {
-    hb_itemRelease( dataBlock );
-    dataBlock = nullptr;
+    hb_itemRelease( m_dataBlock );
+    m_dataBlock = nullptr;
   }
 
-  if( headerDataBlock != nullptr )
+  if( m_headerDataBlock != nullptr )
   {
-    hb_itemRelease( headerDataBlock );
-    headerDataBlock = nullptr;
+    hb_itemRelease( m_headerDataBlock );
+    m_headerDataBlock = nullptr;
   }
 
-  if( flagsBlock != nullptr )
+  if( m_flagsBlock != nullptr )
   {
-    hb_itemRelease( flagsBlock );
-    flagsBlock = nullptr;
+    hb_itemRelease( m_flagsBlock );
+    m_flagsBlock = nullptr;
   }
 
-  if( setDataBlock != nullptr )
+  if( m_setDataBlock != nullptr )
   {
-    hb_itemRelease( setDataBlock );
-    setDataBlock = nullptr;
+    hb_itemRelease( m_setDataBlock );
+    m_setDataBlock = nullptr;
   }
 }
 
 void HAbstractTableModelV2::setRowCountCB( PHB_ITEM block )
 {
-  if( rowCountBlock != nullptr )
+  if( m_rowCountBlock != nullptr )
   {
-    hb_itemRelease( rowCountBlock );
+    hb_itemRelease( m_rowCountBlock );
   }
   if( block != nullptr )
   {
-    rowCountBlock = hb_itemNew( block );
+    m_rowCountBlock = hb_itemNew( block );
   }
 }
 
 void HAbstractTableModelV2::setColumnCountCB( PHB_ITEM block )
 {
-  if( columnCountBlock != nullptr )
+  if( m_columnCountBlock != nullptr )
   {
-    hb_itemRelease( columnCountBlock );
+    hb_itemRelease( m_columnCountBlock );
   }
   if( block != nullptr )
   {
-    columnCountBlock = hb_itemNew( block );
+    m_columnCountBlock = hb_itemNew( block );
   }
 }
 
 void HAbstractTableModelV2::setDataCB( PHB_ITEM block )
 {
-  if( dataBlock != nullptr )
+  if( m_dataBlock != nullptr )
   {
-    hb_itemRelease( dataBlock );
+    hb_itemRelease( m_dataBlock );
   }
   if( block != nullptr )
   {
-    dataBlock = hb_itemNew( block );
+    m_dataBlock = hb_itemNew( block );
   }
 }
 
 void HAbstractTableModelV2::setHeaderDataCB( PHB_ITEM block )
 {
-  if( headerDataBlock != nullptr )
+  if( m_headerDataBlock != nullptr )
   {
-    hb_itemRelease( headerDataBlock );
+    hb_itemRelease( m_headerDataBlock );
   }
   if( block != nullptr )
   {
-    headerDataBlock = hb_itemNew( block );
+    m_headerDataBlock = hb_itemNew( block );
   }
 }
 
 void HAbstractTableModelV2::setFlagsCB( PHB_ITEM block )
 {
-  if( flagsBlock != nullptr )
+  if( m_flagsBlock != nullptr )
   {
-    hb_itemRelease( flagsBlock );
+    hb_itemRelease( m_flagsBlock );
   }
   if( block != nullptr )
   {
-    flagsBlock = hb_itemNew( block );
+    m_flagsBlock = hb_itemNew( block );
   }
 }
 
 void HAbstractTableModelV2::setSetDataCB( PHB_ITEM block )
 {
-  if( setDataBlock != nullptr )
+  if( m_setDataBlock != nullptr )
   {
-    hb_itemRelease( setDataBlock );
+    hb_itemRelease( m_setDataBlock );
   }
   if( block != nullptr )
   {
-    setDataBlock = hb_itemNew( block );
+    m_setDataBlock = hb_itemNew( block );
   }
 }
 
 int HAbstractTableModelV2::rowCount( const QModelIndex & parent ) const
 {
-  if( rowCountBlock != nullptr )
+  if( m_rowCountBlock != nullptr )
   {
     if( parent.isValid() )
     {
@@ -142,7 +142,7 @@ int HAbstractTableModelV2::rowCount( const QModelIndex & parent ) const
     }
     else
     {
-      return (int) hb_itemGetNI( hb_vmEvalBlockV( rowCountBlock, 0 ) );
+      return (int) hb_itemGetNI( hb_vmEvalBlockV( m_rowCountBlock, 0 ) );
     }
   }
   else
@@ -153,7 +153,7 @@ int HAbstractTableModelV2::rowCount( const QModelIndex & parent ) const
 
 int HAbstractTableModelV2::columnCount( const QModelIndex & parent ) const
 {
-  if( columnCountBlock != nullptr )
+  if( m_columnCountBlock != nullptr )
   {
     if( parent.isValid() )
     {
@@ -161,7 +161,7 @@ int HAbstractTableModelV2::columnCount( const QModelIndex & parent ) const
     }
     else
     {
-      return (int) hb_itemGetNI( hb_vmEvalBlockV( columnCountBlock, 0 ) );
+      return (int) hb_itemGetNI( hb_vmEvalBlockV( m_columnCountBlock, 0 ) );
     }
   }
   else
@@ -174,12 +174,12 @@ QVariant HAbstractTableModelV2::data( const QModelIndex & index, int role ) cons
 {
   QVariant data;
 
-  if( dataBlock != nullptr )
+  if( m_dataBlock != nullptr )
   {
     PHB_ITEM pIndex = hb_itemPutPtr( NULL, (QModelIndex *) &index );
     PHB_ITEM pRole = hb_itemPutNI( NULL, role );
 
-    PHB_ITEM pRet = hb_itemNew( hb_vmEvalBlockV( dataBlock, 2, pIndex, pRole ) );
+    PHB_ITEM pRet = hb_itemNew( hb_vmEvalBlockV( m_dataBlock, 2, pIndex, pRole ) );
 
     if( hb_clsIsParent( hb_objGetClass( pRet ), "QVARIANT" ) )
     {
@@ -203,13 +203,13 @@ QVariant HAbstractTableModelV2::headerData( int section, Qt::Orientation orienta
 {
   QVariant data;
 
-  if( headerDataBlock != nullptr )
+  if( m_headerDataBlock != nullptr )
   {
     PHB_ITEM pSection = hb_itemPutNI( NULL, section );
     PHB_ITEM pOrientation = hb_itemPutNI( NULL, (int) orientation );
     PHB_ITEM pRole = hb_itemPutNI( NULL, role );
 
-    PHB_ITEM pRet = hb_itemNew( hb_vmEvalBlockV( headerDataBlock, 3, pSection, pOrientation, pRole ) );
+    PHB_ITEM pRet = hb_itemNew( hb_vmEvalBlockV( m_headerDataBlock, 3, pSection, pOrientation, pRole ) );
 
     if( hb_clsIsParent( hb_objGetClass( pRet ), "QVARIANT" ) )
     {
@@ -234,11 +234,11 @@ Qt::ItemFlags HAbstractTableModelV2::flags( const QModelIndex &index ) const
 {
   Qt::ItemFlags flags = Qt::ItemIsSelectable | Qt::ItemIsEnabled;
 
-  if( flagsBlock != nullptr )
+  if( m_flagsBlock != nullptr )
   {
     PHB_ITEM pIndex = hb_itemPutPtr( NULL, (QModelIndex *) &index );
 
-    PHB_ITEM pRet = hb_itemNew( hb_vmEvalBlockV( flagsBlock, 1, pIndex ) );
+    PHB_ITEM pRet = hb_itemNew( hb_vmEvalBlockV( m_flagsBlock, 1, pIndex ) );
 
     if( hb_itemType( pRet ) & HB_IT_NUMERIC )
     {
@@ -260,13 +260,13 @@ bool HAbstractTableModelV2::setData( const QModelIndex &index, const QVariant &v
 {
   bool success = false;
 
-  if( setDataBlock != nullptr )
+  if( m_setDataBlock != nullptr )
   {
     PHB_ITEM pIndex = hb_itemPutPtr( NULL, (QModelIndex *) &index );
     PHB_ITEM pValue = hb_itemPutPtr( NULL, (QVariant *) &value );
     PHB_ITEM pRole = hb_itemPutNI( NULL, role );
 
-    PHB_ITEM pRet = hb_itemNew( hb_vmEvalBlockV( setDataBlock, 3, pIndex, pValue, pRole ) );
+    PHB_ITEM pRet = hb_itemNew( hb_vmEvalBlockV( m_setDataBlock, 3, pIndex, pValue, pRole ) );
 
     if( hb_itemType( pRet ) & HB_IT_LOGICAL )
     {
@@ -289,11 +289,11 @@ bool HAbstractTableModelV2::setData( const QModelIndex &index, const QVariant &v
 void HAbstractTableModelV2::reloadData()
 {
   // Notas da documentação do Qt:
-  emit QAbstractTableModel::layoutAboutToBeChanged();
+  emit QAbstractItemModel::layoutAboutToBeChanged();
   // Remember the QModelIndex that will change
   // Update your internal data
   // Call changePersistentIndex()
   // emit layoutChanged
   //emit QAbstractItemModel::layoutChanged();
-  emit QAbstractTableModel::layoutChanged();
+  emit QAbstractItemModel::layoutChanged();
 }
