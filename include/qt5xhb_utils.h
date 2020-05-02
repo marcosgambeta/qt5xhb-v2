239cb8cd9d77ef369d32f1ec6ea146ebf9d07ef3
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2020-05-02 10:14:58 -0300

    New internal functions:
    PHB_ITEM returnQModelIndexObject( void * ptr )
    PHB_ITEM returnQVariantObject( void * ptr )
    
    Modified:
    include/qt5xhb_utils.h
    source/qt5xhb_utils.cpp

--- include/qt5xhb_utils.h
+++ include/qt5xhb_utils.h
@@ -55,4 +55,7 @@ namespace Qt5xHb
   QVariantList convert_array_parameter_to_qvariantlist( int numpar );
   void convert_qvariantlist_to_array( const QVariantList list );
   //void _qt5xhb_convert_qlist_qbluetoothuuid_to_array ( const QList<QBluetoothUuid> list );
+
+  PHB_ITEM returnQModelIndexObject( void * ptr );
+  PHB_ITEM returnQVariantObject( void * ptr );
 }
