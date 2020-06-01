0c1f8c2d54d3c53a9f16db06da39513b3ac8c72d
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2020-06-01 07:39:27 -0300

    source/version.cpp: updated

--- source/version.cpp
+++ source/version.cpp
@@ -12,12 +12,12 @@
 
 HB_FUNC( QT5XHB_VERSION )
 {
-  hb_retc( (const char *) "2020.05" );
+  hb_retc( (const char *) "2020.06" );
 }
 
 HB_FUNC( QT5XHB_VERSION_DATE )
 {
-  hb_retc( (const char *) "30/05/2020 21:45:00" );
+  hb_retc( (const char *) "01/06/2020 07:30:00" );
 }
 
 HB_FUNC( QT5XHB_ISCPP11 )
@@ -31,12 +31,12 @@ HB_FUNC( QT5XHB_ISCPP11 )
 
 HB_FUNC( QT4XHB_VERSION )
 {
-  hb_retc( (const char *) "2020.05" );
+  hb_retc( (const char *) "2020.06" );
 }
 
 HB_FUNC( QT4XHB_VERSION_DATE )
 {
-  hb_retc( (const char *) "30/05/2020 21:45:00" );
+  hb_retc( (const char *) "01/06/2020 07:30:00" );
 }
 
 /*
